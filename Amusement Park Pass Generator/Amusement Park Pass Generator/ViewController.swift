d85028a104ee0b30ab7bc00b943f8d62ccc6f729
Natalie Stimpson <n-stimpson@hotmail.com>
2018-11-09 17:49:00 +0000

    updated UI
    aaded the UI elements

--- Amusement Park Pass Generator/Amusement Park Pass Generator/ViewController.swift
+++ Amusement Park Pass Generator/Amusement Park Pass Generator/ViewController.swift
@@ -2,16 +2,97 @@ import UIKit
 import Foundation
 
 class ViewController: UIViewController {
-
+    /*
+    //connect the top row of buttons
+    @IBOutlet weak var guestButton: UIButton!
+    @IBOutlet weak var employeeButton: UIButton!
+    @IBOutlet weak var managerButton: UIButton!
+    @IBOutlet weak var vendorButton: UIButton!
+    
+     //connect the second row of buttons
+    @IBOutlet weak var subTypeOne: UIButton!
+    @IBOutlet weak var subTypeTwo: UIButton!
+    @IBOutlet weak var subTypeThree: UIButton!
+    @IBOutlet weak var subTypeFour: UIButton!
+    
+    //connect the first row of data
+    @IBOutlet weak var dobLabel: UILabel!
+    @IBOutlet weak var dobField: UITextField!
+    @IBOutlet weak var ssnLabel: UILabel!
+    @IBOutlet weak var ssnField: UITextField!
+    @IBOutlet weak var projectLabel: UILabel!
+    @IBOutlet weak var projectField: UITextField!
+    
+    //connect the second row of data
+    @IBOutlet weak var firstNameLabel: UILabel!
+    @IBOutlet weak var firstNameField: UITextField!
+    @IBOutlet weak var lastNameLabel: UILabel!
+    @IBOutlet weak var lastNameField: UITextField!
+    
+    //connect the fourth row of data
+    @IBOutlet weak var companyLabel: UILabel!
+    @IBOutlet weak var companyField: UITextField!
+    
+    //connect the fifth row of data
+    @IBOutlet weak var streetAddressLabel: UILabel!
+    @IBOutlet weak var streetAddressField: UITextField!
+    
+    //connect the sixth row of data
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var cityField: UITextField!
+    @IBOutlet weak var stateLabel: UILabel!
+    @IBOutlet weak var stateField: UITextField!
+    @IBOutlet weak var zipLabel: UILabel!
+    @IBOutlet weak var zipField: UITextField!
+    
+    //connect bottom buttons
+    @IBOutlet weak var generatePassButton: UIButton!
+    @IBOutlet weak var populateDataButton: UIButton!
+    
+    */
+    //override with XYZ actions
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
+        //configureTextFields()
     }
 
 
+   /* private func configureTextFields() {
+        dobField.delegate = self
+        ssnField.delegate = self
+        projectField.delegate = self
+        firstNameField.delegate = self
+        lastNameField.delegate = self
+        companyField.delegate = self
+        streetAddressField.delegate = self
+        cityField.delegate = self
+        stateField.delegate = self
+        zipField.delegate = self
+    } */
+    
+    
+    
+   /* private func configureTapGesture() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(ViewController.handleTap))
+    }
+    
+    @objc func handleTap() {
+    //line below copy and place in any buttons 
+        view.endEditing(true)
+    } */
+}
+    
+    /*
+
+//allows keyboard to be dismissed when user hits 'return'
+extension ViewController : UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 }
 
-
-
+*/
 
 
